42574fc5fa0d151cac2de82491bbfcaafa0844dd
asdflkj169 <asdflkj169@gmail.com>
2024-11-18 01:34:08 +0900

    Angular の設定変更 project を分けて片方はzoneless にした。

--- src/app/app.zoneless.component.ts
+++ src/app/app.zoneless.component.ts
@@ -8,6 +8,6 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'test-zoneless_zone';
+export class AppZonelessComponent {
+  title = 'test-zoneless_zoneless';
 }
---
+++ src/main.zoneless.ts
@@ -0,0 +1,6 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.zoneless.config';
+import { AppZonelessComponent } from "./app/app.zoneless.component";
+
+bootstrapApplication(AppZonelessComponent, appConfig)
+  .catch((err) => console.error(err));
