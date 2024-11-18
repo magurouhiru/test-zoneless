1f5c2e71f4690b5e3ac2b7e645ba0843348b89f4
asdflkj169 <asdflkj169@gmail.com>
2024-11-18 14:23:13 +0900

    add prettier

--- src/main.zoneless.ts
+++ src/main.zoneless.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.zoneless.config';
-import { AppZonelessComponent } from "./app/app.zoneless.component";
+import { AppZonelessComponent } from './app/app.zoneless.component';
 
-bootstrapApplication(AppZonelessComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppZonelessComponent, appConfig).catch((err) =>
+  console.error(err),
+);
