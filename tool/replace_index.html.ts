405f18aca1c4dc452b7e964114a441fed7a827e9
asdflkj169 <asdflkj169@gmail.com>
2024-11-19 22:17:20 +0900

    test

--- tool/replace_index.html.ts
+++ tool/replace_index.html.ts
@@ -1,4 +1,4 @@
-import { $ } from 'execa';
+import { execa } from 'execa';
 import * as fs from 'node:fs';
 
 const input = './index.html';
@@ -6,7 +6,7 @@ const output = './dist/index.html';
 const target = 'replace.me';
 
 Promise.all([
-  $('npm ls'),
+  execa`npm ls`,
   fs.promises.readFile(input, { encoding: 'utf8' }),
 ]).then(([e, f]) => {
   const text = f.replace(target, e.stdout.trim());
