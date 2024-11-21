ec806edddc9e197a257e2ccc4d70d45c26efc1be
magurouhiru <73399968+magurouhiru@users.noreply.github.com>
2024-11-21 21:59:18 +0900

    19 add test asyncpipe component (#20)
    * コンポーネント追加
    
    * add test async pipe component
    close #19

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,19 @@
 import { Routes } from '@angular/router';
 import { TestClickEventAndObservableNextComponent } from './test-click-event-and-observable-next/test-click-event-and-observable-next.component';
+import { TestAsyncPipeComponent } from './test-async-pipe/test-async-pipe.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: TestClickEventAndObservableNextComponent,
+    component: HomeComponent,
   },
   {
     path: 'test-click-event-and-observable-next',
     component: TestClickEventAndObservableNextComponent,
+  },
+  {
+    path: 'test-async-pipe',
+    component: TestAsyncPipeComponent,
   },
 ];
---
+++ src/app/test-async-pipe/test-async-pipe.component.ts
@@ -0,0 +1,69 @@
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  inject,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { NoSignal01Component } from '../test-click-event-and-observable-next/no-signal-01/no-signal-01.component';
+import { Signal01Component } from '../test-click-event-and-observable-next/signal-01/signal-01.component';
+import { take, interval } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { CntService } from '../cnt.service';
+
+@Component({
+  selector: 'app-test-async-pipe',
+  standalone: true,
+  imports: [NoSignal01Component, Signal01Component, AsyncPipe],
+  templateUrl: './test-async-pipe.component.html',
+  styleUrl: './test-async-pipe.component.scss',
+})
+export class TestAsyncPipeComponent
+  implements
+    OnChanges,
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  name = 'test-async-pipe';
+  label = '';
+
+  interval = interval(1000).pipe(take(5));
+
+  // ライフサイクル監視
+  cntService = inject(CntService);
+  ngOnChanges(changes: SimpleChanges) {}
+  ngOnInit() {
+    this.label = this.name;
+    this.cntService.setData(this.label);
+    this.cntService.addOnInit(this.label);
+  }
+  ngDoCheck() {
+    this.cntService.addDoCheck(this.label);
+  }
+  ngAfterContentInit() {
+    this.cntService.addAfterContentInit(this.label);
+  }
+  ngAfterContentChecked() {
+    this.cntService.addAfterContentChecked(this.label);
+  }
+  ngAfterViewInit() {
+    this.cntService.addAfterViewInit(this.label);
+  }
+  ngAfterViewChecked() {
+    this.cntService.addAfterViewChecked(this.label);
+  }
+  ngOnDestroy() {
+    this.cntService.addOnDestroy(this.label);
+  }
+}
