19a9cf79263b175cfae00eca67a430d7fc75d04f
asdflkj169 <asdflkj169@gmail.com>
2024-11-21 22:05:39 +0900

    コンポーネント追加

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { TestClickEventAndObservableNextComponent } from './test-click-event-and-observable-next/test-click-event-and-observable-next.component';
 import { TestAsyncPipeComponent } from './test-async-pipe/test-async-pipe.component';
 import { HomeComponent } from './home/home.component';
+import { TestControlFlowSyntaxComponent } from './test-control-flow-syntax/test-control-flow-syntax.component';
 
 export const routes: Routes = [
   {
@@ -15,5 +16,9 @@ export const routes: Routes = [
   {
     path: 'test-async-pipe',
     component: TestAsyncPipeComponent,
+  },
+  {
+    path: 'test-control-flow-syntax',
+    component: TestControlFlowSyntaxComponent,
   },
 ];
---
+++ src/app/test-control-flow-syntax/test-control-flow-syntax.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-control-flow-syntax',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-control-flow-syntax.component.html',
+  styleUrl: './test-control-flow-syntax.component.scss',
+})
+export class TestControlFlowSyntaxComponent {
+  name = 'test-control-flow-syntax';
+  label = '';
+}
