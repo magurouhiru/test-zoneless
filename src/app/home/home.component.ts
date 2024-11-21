3cedd5afcfc9133bd12300e231683d6245c2ecf4
asdflkj169 <asdflkj169@gmail.com>
2024-11-21 21:43:23 +0900

    コンポーネント追加

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  links: { link: string; label: string }[] = [
+    {
+      link: 'test-click-event-and-observable-next',
+      label: 'test-click-event-and-observable-next',
+    },
+    { link: 'test-async-pipe', label: 'test-async-pipe' },
+  ];
+}
---
+++ src/app/test-async-pipe/test-async-pipe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-async-pipe',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-async-pipe.component.html',
+  styleUrl: './test-async-pipe.component.scss',
+})
+export class TestAsyncPipeComponent {}
